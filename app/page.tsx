80ecdab6b0066b98473491d7a8f6997b6a610817
Praveena-Krishnan <praveenakrishnan2003@gmail.com>
2024-12-05 22:37:49 +0530

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,100 +1,135 @@
 import Image from "next/image";
+import Link from "next/link";
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <div className="bg-white">
+      {/* Navbar */}
+      <nav className="bg-white shadow-md py-4 ">
+        <div className="container mx-auto flex justify-between items-center px-4 ">
+          <div className="flex items-center">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src="https://websitedemos.net/food-blogger-04/wp-content/uploads/sites/793/2021/03/food-blogger-site-logo.svg"
+              alt="Logo"
+              width={64}
+              height={34}
+              className="ml-32 "
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
+          <ul className="flex space-x-8 text-lg font-semibold mr-32">
+            <li><Link href="/"className="text-black hover:text-amber-700 cursor-pointer">Home</Link></li>
+            <li className="text-black hover:text-amber-700 cursor-pointer">About</li>
+            <li className="text-black hover:text-amber-700 cursor-pointer">Reviews</li>
+            <li className="text-black hover:text-amber-700 cursor-pointer">Contact</li>
+          </ul>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      </nav>
+
+      {/* Hero Section */}
+      <div className="relative flex items-center justify-between bg-white pt-20 pb-32 px-48">
+        <div className="flex flex-col max-w-lg">
+          <h2 className="text-amber-600 text-lg font-semibold tracking-wider">
+            ________WELCOME
+          </h2>
+          <h1 className="text-5xl font-serif text-black font-bold mt-6">
+            I'm Dianna Adams
+          </h1>
+          <p className="text-3xl text-black font-semibold mt-4">Food Critic / Influencer / Blogger</p>
+          <p className="text-xl text-gray-400 font-light mt-6">
+            Irure nisi do reprehenderit officia sint adipisicing excepteur occaecat eiusmod. Dolore Lorem eiusmod consectetur amet dolor cupidatat sunt. Eu dolor aute nisi nisi.
+          </p>
+          <button className="bg-white border border-amber-700 text-amber-700 font-medium text-lg py-2 px-6 mt-8 rounded">
+            Join Me
+          </button>
+        </div>
+        <div className="w-1/2">
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src="/images/hero.png"  
+            alt="Food Blogger"
+            width={600}
+            height={500}
+            className="rounded-lg shadow-lg"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        </div>
+      </div>
+
+      {/* Latest Blogs Section */}
+      <div className="container mx-auto px-4 py-8">
+        <h2 className="text-4xl font-bold font-serif text-left mb-10 text-black ml-20">Latest Blogs</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 mr-20 ml-20">
+          {/* Blog Post 1 */}
+          <div className="bg-white  overflow-hidden">
+            <Image
+              src="/images/b1.png" 
+              alt="Blog 1"
+              width={500}
+              height={300}
+              className="w-full h-56 object-cover"
+            />
+            <div className="p-6">
+              <h3 className="text-2xl font-bold text-black">Garlic Butter White Wine Shrimp</h3>
+              <p className="text-gray-500 mt-4">
+                Commodo labore Lorem consequat et.Commodo labore Lorem consequat et.
+              </p>
+              <button className="text-amber-700 hover:text-amber-900 mt-6 border border-amber-700 p-2">
+                Read More
+              </button>
+            </div>
+          </div>
+          {/* Blog Post 2 */}
+          <div className="bg-white   overflow-hidden">
+            <Image
+              src="/images/b2.png" 
+              alt="Blog 2"
+              width={500}
+              height={300}
+              className="w-full h-56 object-cover"
+            />
+            <div className="p-6">
+              <h3 className="text-2xl font-bold text-black">Grilled Flank Steak with Chimichurri</h3>
+              <p className="text-gray-500 mt-4">
+              Commodo labore Lorem consequat et.Commodo labore Lorem consequat et.
+              </p>
+              <button className="text-amber-700 hover:text-amber-900 mt-6 border border-amber-700 p-2">
+                Read More
+              </button>
+            </div>
+          </div>
+          {/* Blog Post 3 */}
+          <div className="bg-white   overflow-hidden">
+            <Image
+              src="/images/b3.png" 
+              alt="Blog 3"
+              width={500}
+              height={300}
+              className="w-full h-56 object-cover"
+            />
+            <div className="p-6">
+              <h3 className="text-2xl font-bold text-black">Mushroom Penne with Walnut Pesto</h3>
+              <p className="text-gray-500 mt-4">
+              Commodo labore Lorem consequat et.Commodo labore Lorem consequat et.
+              </p>
+              <button className="text-amber-700 hover:text-amber-900 mt-6 border border-amber-700 p-2">
+                Read More
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Footer */}
+      <footer className="bg-black text-white py-8">
+        <div className="container mx-auto text-center">
+          <p className="text-sm">
+            &copy; 2024 Dianna Adams. All rights reserved.
+          </p>
+          <div className="flex justify-center space-x-6 mt-4">
+            <a href="#" className="text-amber-700 hover:text-amber-500">Facebook</a>
+            <a href="#" className="text-amber-700 hover:text-amber-500">Twitter</a>
+            <a href="#" className="text-amber-700 hover:text-amber-500">Instagram</a>
+          </div>
+        </div>
       </footer>
     </div>
   );
