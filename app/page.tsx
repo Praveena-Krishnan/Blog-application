e5356e034c4ce4bd21c3bcb88a3b4fcd86303ccc
Praveena-Krishnan <praveenakrishnan2003@gmail.com>
2024-12-06 21:57:41 +0530

    footer

--- app/page.tsx
+++ app/page.tsx
@@ -85,8 +85,28 @@ export default function Home() {
           
         </div>
       </div>
-
-     
-    </div>
+      {/* Latest Blogs Section */}
+      {/* footer */} 
+      <div className="container mx-auto flex justify-between items-center px-4 mt-20 p-10 bg-red-200">
+          <div className="flex items-center">
+            <Image
+              src="https://websitedemos.net/food-blogger-04/wp-content/uploads/sites/793/2021/03/food-blogger-site-logo.svg"
+              alt="Logo"
+              width={64}
+              height={34}
+              className="ml-32 "
+            />
+          </div>
+          <ul className="flex space-x-8 text-xs font-semibold mr-32">
+            <li><Link href="/"className="text-black hover:text-amber-700 cursor-pointer">Home</Link></li>
+            <li className="text-black hover:text-amber-700 cursor-pointer">About</li>
+            <li className="text-black hover:text-amber-700 cursor-pointer">Reviews</li>
+            <li className="text-black hover:text-amber-700 cursor-pointer">Contact</li>
+          </ul>
+      <div>
+        <p className="flex ">© 2024 Food Blogger & Influencer</p>
+      </div>
+        </div>
+        </div>
   );
 }
